f08856c0624183cd5c4432dd36dc55109254a75a
HectorFunesInteramercon <118022293+HectorFunesInteramercon@users.noreply.github.com>
2023-07-12 23:22:20 -0600

    4 Conexion
    Clase para conectar a base de datos

---
+++ 4 CONEXION/Conexion/Conexion/conectarBaseDatos.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Conexion
+{
+    public class conectarBaseDatos
+    {
+
+        private string connectionString;
+
+        public conectarBaseDatos(string connectionString)
+        {
+            this.connectionString = connectionString;
+        }
+
+        public DataSet ejecutarSelect(string query)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                SqlDataAdapter adapter = new SqlDataAdapter(query, connection);
+                DataSet dataSet = new DataSet();
+                adapter.Fill(dataSet);
+                return dataSet;
+            }
+        }
+
+        public int contarResultadoUpdateDelete(string query)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                SqlCommand command = new SqlCommand(query, connection);
+                int rowsAffected = command.ExecuteNonQuery();
+                return rowsAffected;
+            }
+        }
+
+        public int contarEnSelect(string query)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                SqlCommand command = new SqlCommand(query, connection);
+                object result = command.ExecuteScalar();
+                int rowCount = Convert.ToInt32(result);
+                return rowCount;
+            }
+        }
+    }
+}
